610124c82a8fa5da8c12358172dbb4b842d20a1e
Mamadou lamine Bèye <laminebeye007@gmail.com>
2022-01-02 12:45:41 +0000

    changing client seeder

--- database/migrations/2021_12_07_153832_create_clients_table.php
+++ database/migrations/2021_12_07_153832_create_clients_table.php
@@ -14,8 +14,9 @@ class CreateClientsTable extends Migration
     public function up()
     {
         Schema::create('clients', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->foreignId('user_id')->references('id')->on('users')->unique();
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->primary(['id','user_id']);
             $table->string('pays_residence');
             $table->integer('solde');
             $table->timestamps();
